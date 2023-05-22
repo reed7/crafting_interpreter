5dafc9ab131178342fd61166d4f77deb83e2396b
wei.zhong <wei.zhong@servicenow.com>
2023-05-22 09:02:14 -0400

    init commit

---
+++ src/main/java/org/greencable/jlox/Scanner.java
@@ -0,0 +1,31 @@
+package org.greencable.jlox;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Scanner {
+    private final String source;
+    private final List<Token> tokens = new ArrayList<>();
+    private int start = 0;
+    private int current = 0;
+    private int line = 1;
+
+    Scanner(String source) {
+        this.source = source;
+    }
+
+    List<Token> scanTokens() {
+        while (!isAtEnd()) {
+        // We are at the beginning of the next lexeme.
+        start = current;
+        scanToken();
+        }
+
+        tokens.add(new Token(TokenType.EOF, "", null, line));
+        return tokens;
+    }
+
+    private boolean isAtEnd() {
+        return current >= source.length();
+    }
+}
